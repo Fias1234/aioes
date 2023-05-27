b6081ee424f8878c20c5900fc5b83d0037a77798
Fias1234 <134765330+Fias1234@users.noreply.github.com>
2023-05-27 13:31:25 +0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,16 +3,22 @@ function toggleLanguage() {
     var headingUrdu = document.getElementById("heading-urdu");
     var subheadingEnglish = document.getElementById("subheading-english");
     var subheadingUrdu = document.getElementById("subheading-urdu");
-
+    var MissionHeadEng = document.getElementById("Mission Head Eng");
+    var MissionHeadUrdu = document.getElementById("Mission Head Urdu");
+    
     if (headingEnglish.style.display === "none") {
         headingEnglish.style.display = "block";
         headingUrdu.style.display = "none";
         subheadingEnglish.style.display = "block";
         subheadingUrdu.style.display = "none";
+        MissionHeadEng.style.display = "block";
+        MissionHeadUrdu.style.display = "none";
     } else {
         headingEnglish.style.display = "none";
         headingUrdu.style.display = "block";
         subheadingEnglish.style.display = "none";
         subheadingUrdu.style.display = "block";
+        MissionHeadEng.style.display = "none";
+        MissionHeadUrdu.style.display = "block";
     }
 }
