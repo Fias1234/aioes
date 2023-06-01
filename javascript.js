ffd98e6ffe308b0c8b44f904c9db73c14d8751c4
Fias1234 <134765330+Fias1234@users.noreply.github.com>
2023-06-01 12:56:55 +0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,28 +1,12 @@
 function toggleLanguage() {
-    var headingEnglish = document.getElementById("heading-english");
-    var headingUrdu = document.getElementById("heading-urdu");
-    var subheadingEnglish = document.getElementById("subheading-english");
-    var subheadingUrdu = document.getElementById("subheading-urdu");
-    var MissionHeadEng = document.getElementById("Mission Head Eng");
-    var MissionHeadUrdu = document.getElementById("Mission Head Urdu");
+    var Eng = document.getElementById("Eng");
+    var Urdu = document.getElementById("Urdu");
     
-    if (headingEnglish.style.display === "none") {
-        headingEnglish.style.display = "block";
-        headingUrdu.style.display = "none";
-        subheadingEnglish.style.display = "block";
-        subheadingUrdu.style.display = "none";
-        MissionHeadEng.style.display = "block";
-        MissionHeadUrdu.style.display = "none";
-        Mission Eng.style.display = "block";
-        Mission Urdu.style.display = "none";
+    if (Eng.style.display === "none") {
+        Eng.style.display = "block";
+        Urdu.style.display = "none";
     } else {
-        headingEnglish.style.display = "none";
-        headingUrdu.style.display = "block";
-        subheadingEnglish.style.display = "none";
-        subheadingUrdu.style.display = "block";
-        MissionHeadEng.style.display = "none";
-        MissionHeadUrdu.style.display = "block";
-        Mission Eng.style.display = "none";
-        Mission Urdu.style.display = "block";
+        Eng.style.display = "none";
+        Urdu.style.display = "block";
     }
 }
